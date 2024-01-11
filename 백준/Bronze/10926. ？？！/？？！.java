0495dee92ea647a54e7406ecceda90b99f3f9900
koongFan <131753512+koongFan@users.noreply.github.com>
2024-01-11 21:38:55 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+            // 백준 10926 번
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println(scanner.next() + "??!");
+
+            scanner.close();
+    }
+
+}
