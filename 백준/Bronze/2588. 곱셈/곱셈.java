4a1837301f03d853d6f30d51f6adf2c6c5ff6ff1
koongFan <131753512+koongFan@users.noreply.github.com>
2024-01-11 23:42:05 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 16476 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -14,26 +14,27 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
             // 백준 2588 번
-		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		// StringTokenizer st = new StringTokenizer(br.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // int firstValue = Integer.parseInt(st.nextToken());
-        // String secondValue =  st.nextToken();
+        int firstValue = Integer.parseInt(br.readLine());
+        String secondValue =  br.readLine();
+            int firstOfSecondValue = Character.getNumericValue(secondValue.charAt(secondValue.length()-3));
+            int secondOfSecondValue = Character.getNumericValue(secondValue.charAt(secondValue.length()-2));
+            int thirdOfSecondValue = Character.getNumericValue(secondValue.charAt(secondValue.length()-1));
 
-        // bw.write(firstValue*Character.getNumericValue(secondValue.charAt(secondValue.length()-1)) + "\n" + firstValue*Character.getNumericValue(secondValue.charAt(secondValue.length()-2))+ "\n" + firstValue*Character.getNumericValue(secondValue.charAt(secondValue.length()-3)) + "\n" + firstValue*Integer.parseInt(secondValue) );
+        bw.write(firstValue*thirdOfSecondValue 
+                + "\n" 
+                + firstValue*secondOfSecondValue
+                + "\n" 
+                + firstValue*firstOfSecondValue 
+                + "\n" 
+                + firstValue*Integer.parseInt(secondValue) 
+                );
 
-        // bw.flush();
-        // bw.close();
-        // br.close();
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        String B = String.valueOf(sc.nextInt());
-
-        System.out.println(A*Character.getNumericValue(B.charAt(B.length()-1)));
-        System.out.println(A*Character.getNumericValue(B.charAt(B.length()-2)));
-        System.out.println(A*Character.getNumericValue(B.charAt(B.length()-3)));
-        System.out.println(A*Integer.parseInt(B));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
 }
