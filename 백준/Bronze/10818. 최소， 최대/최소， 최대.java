c6304f4ded802679a50045a71a856d4290a8bd1d
koongFan <131753512+koongFan@users.noreply.github.com>
2024-01-17 12:55:42 +0900

    [Bronze III] Title: 최소, 최대, Time: 1388 ms, Memory: 106364 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+
+        int min = arr[0];
+        int max = arr[N-1];
+
+        bw.write(String.valueOf(min) + " " + String.valueOf(max));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+}
